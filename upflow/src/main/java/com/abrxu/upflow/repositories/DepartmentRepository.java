33585ade8f28a4ef10c89090cde77e82508bf533
abrxu <abrxu@yahoo.com>
2025-05-11 01:15:33 -0300

    add: created DepartmentRepository and DepartmentService

---
+++ upflow/src/main/java/com/abrxu/upflow/repositories/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.abrxu.upflow.repositories;
+
+import com.abrxu.upflow.models.department.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
---
+++ upflow/src/main/java/com/abrxu/upflow/services/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.abrxu.upflow.services;
+
+import com.abrxu.upflow.models.department.Department;
+import com.abrxu.upflow.repositories.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Department not found."));
+    }
+}
