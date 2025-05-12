1e2d93116188354a6820d3e15bb751389048c858
abrxu <abrxu@yahoo.com>
2025-05-12 03:07:36 -0300

    fix: removed wrong Transactional import and work in progress in method createDepartment

--- upflow/src/main/java/com/abrxu/upflow/department/services/DepartmentManagementService.java
+++ upflow/src/main/java/com/abrxu/upflow/department/services/DepartmentManagementService.java
@@ -2,30 +2,47 @@ package com.abrxu.upflow.department.services;
 
 import com.abrxu.upflow.department.domain.Department;
 import com.abrxu.upflow.department.dtos.DepartmentCreationDTO;
+import com.abrxu.upflow.department.repositories.DepartmentRepository;
+import com.abrxu.upflow.user.domain.User;
 import com.abrxu.upflow.user.services.UserService;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class DepartmentManagementService {
 
     final UserService userService;
+    final DepartmentRepository departmentRepository;
 
-    public DepartmentManagementService(UserService userService) {
+    public DepartmentManagementService(UserService userService, DepartmentRepository departmentRepository) {
         this.userService = userService;
+        this.departmentRepository = departmentRepository;
     }
 
+    // TODO: create a Department response DTO to return the department with users.
     @Transactional
     public Department createDepartment(DepartmentCreationDTO dto) {
-        var department = new Department();
-        var users = userService.findUsersByIds(dto.usersIds());
-        var manager = userService.findUserById(dto.managerId());
-        department.setUsers(users);
-        department.setManager(manager);
-        BeanUtils.copyProperties(dto, department);
 
-        return department;
+        if (dto.usersIds().contains(dto.managerId())) throw new RuntimeException("Manager cannot be also a common user.");
+
+        var department = new Department();
+        var manager = userService.findUserById(dto.managerId());
+        BeanUtils.copyProperties(dto, department, "managerId", "usersIds");
+
+        department.setManager(manager);
+        manager.setDepartment(department);
+
+        List<Long> ids = dto.usersIds();
+        if (ids != null && !ids.isEmpty()) {
+            List<User> users = userService.findUsersByIds(ids);
+            users.forEach(user -> user.setDepartment(department));
+            department.setUsers(users);
+        }
+
+        return departmentRepository.save(department);
     }
 
 }
--- upflow/src/main/java/com/abrxu/upflow/department/services/DepartmentService.java
+++ upflow/src/main/java/com/abrxu/upflow/department/services/DepartmentService.java
@@ -1,11 +1,8 @@
 package com.abrxu.upflow.department.services;
 
 import com.abrxu.upflow.department.domain.Department;
-import com.abrxu.upflow.department.dtos.DepartmentCreationDTO;
 import com.abrxu.upflow.department.repositories.DepartmentRepository;
 import com.abrxu.upflow.user.services.UserService;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
