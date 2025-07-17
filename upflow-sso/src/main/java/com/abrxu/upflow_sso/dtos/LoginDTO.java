e2d9f84f9a8daa9d5a4c23e981a7ae604d373514
abrxu <abrxu@yahoo.com>
2025-07-17 01:28:15 -0300

    refactor: deleted stuff that we'll use just in the future (auth things)

--- upflow-sso/src/main/java/com/abrxu/upflow_sso/dtos/LoginDTO.java
+++
@@ -1,4 +0,0 @@
-package com.abrxu.upflow_sso.dtos;
-
-public record LoginDTO(String username, String password) {
-}
--- upflow-sso/src/main/java/com/abrxu/upflow_sso/dtos/UserDTO.java
+++
@@ -1,4 +0,0 @@
-package com.abrxu.upflow_sso.dtos;
-
-public record UserDTO(String username, String password, String email) {
-}
