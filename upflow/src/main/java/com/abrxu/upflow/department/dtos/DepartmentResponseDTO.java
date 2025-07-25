f1c90e585b18a13149467d5946de256f038fc4b3
abrxu <abrxu@yahoo.com>
2025-07-25 02:03:20 -0300

    refactor: removed unused entities (ill add it in the future again)

--- upflow/src/main/java/com/abrxu/upflow/department/dtos/DepartmentResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.abrxu.upflow.department.dtos;
-
-public record DepartmentResponseDTO(
-
-
-
-) {
-}
--- upflow/src/main/java/com/abrxu/upflow/department/repositories/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abrxu.upflow.department.repositories;
-
-import com.abrxu.upflow.department.domain.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
