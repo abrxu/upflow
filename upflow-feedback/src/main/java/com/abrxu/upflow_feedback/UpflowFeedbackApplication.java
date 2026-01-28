6965e40da2a9d202ed4ac050581dff59c0d2a3fd
abrxu <abrxu@yahoo.com>
2026-01-28 00:42:21 -0300

    adding upflow-feedback service

---
+++ upflow-feedback/src/main/java/com/abrxu/upflow_feedback/UpflowFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.abrxu.upflow_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpflowFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpflowFeedbackApplication.class, args);
+	}
+
+}
---
+++ upflow-feedback/src/test/java/com/abrxu/upflow_feedback/UpflowFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrxu.upflow_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpflowFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
